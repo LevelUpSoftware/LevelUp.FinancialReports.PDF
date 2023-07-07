af5350a7f4e622905849d5e0313a930be09dde8c
realmvpisme <56960488+realmvpisme@users.noreply.github.com>
2023-07-07 11:43:31 -0500

    Fixed Account Nesting

--- LevelUp.FinancialReports.PDF.TestApp/Program.cs
+++ LevelUp.FinancialReports.PDF.TestApp/Program.cs
@@ -8,10 +8,11 @@ var balanceSheetReport = new BalanceSheetReport("Jellystone Park", ReportMonth.A
 
 var currentAssetsAccount = new BalanceSheetAccount("Current Assets", BalanceSheetAccountType.Asset);
 currentAssetsAccount.AddChildAccount(new BalanceSheetAccount("Petty Cash", BalanceSheetAccountType.Asset, 5500.21M));
-
+currentAssetsAccount.AddChildAccount(new BalanceSheetAccount("Picnic Basket Fund", BalanceSheetAccountType.Asset, 756.21M, 2100));
 balanceSheetReport.AddAccount(currentAssetsAccount);
-balanceSheetReport.AddAccount(new BalanceSheetAccount("Picnic Basket Fund", BalanceSheetAccountType.Asset, 756.21M, 2100));
+
 balanceSheetReport.AddAccount(new BalanceSheetAccount("Park Ranger Fines", BalanceSheetAccountType.Liability, 2752.63M));
 
 var pdf = balanceSheetReport.Compose();
-File.WriteAllBytes("C:\\Users\\NathanBurdick\\Downloads\\TestPdf.pdf", pdf);
+var userHomePath = Environment.GetEnvironmentVariable("HOMEPATH");
+File.WriteAllBytes($"{userHomePath}\\Downloads\\TestPdf.pdf", pdf);
--- LevelUp.FinancialReports.PDF/Extensions/ColumnDescriptorExtensions.cs
+++ LevelUp.FinancialReports.PDF/Extensions/ColumnDescriptorExtensions.cs
@@ -6,7 +6,7 @@ namespace LevelUp.FinancialReports.PDF.Extensions;
 
 public static class ColumnDescriptorExtensions
 {
-    public static void CreateRow(this ColumnDescriptor column, BalanceSheetAccount account, bool accountNumbersExist, short fontSize)
+    public static void CreateRow(this ColumnDescriptor column, BalanceSheetAccount account, bool accountNumbersExist, short fontSize, int nestLevel = 0)
     {
         column.Item().Row(row =>
         {
@@ -15,6 +15,11 @@ public static class ColumnDescriptorExtensions
                 var maxAccountNumberLength = 75;
                 row.ConstantItem(maxAccountNumberLength)
                     .Text(account.AccountNumber?.ToString() ?? "--").FontSize(fontSize);
+            }
+
+            if (nestLevel > 0)
+            {
+                row.ConstantItem(5 * nestLevel).AlignLeft().Text(string.Empty);
             }
 
             if (account.Balance.HasValue)
@@ -26,12 +31,14 @@ public static class ColumnDescriptorExtensions
                 row.ConstantItem(125).AlignLeft().Text(account.Name).FontSize(fontSize).Bold();
             }
 
+           
+
             row.ConstantItem(50).AlignLeft().Text(account.Balance.ToString()).FontSize(fontSize);
             row.RelativeItem().EnsureSpace();
 
             foreach (var childAccount in account.ChildAccounts)
             {
-                CreateRow(column, childAccount, accountNumbersExist, fontSize);
+                CreateRow(column, childAccount, accountNumbersExist, fontSize, nestLevel + 1);
             }
         });
     }
